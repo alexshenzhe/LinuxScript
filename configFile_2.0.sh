4b26c1a87dada908cd0872f7a37d956a7a3d0a11
SEN_ZE <alexshenzhe@sina.com>
2018-02-10 18:17:58 +0800

    代码重构

--- configFile_2.0.sh
+++ configFile_2.0.sh
@@ -36,249 +36,207 @@ shutdown_portGeo=8227
 http_portGeo=8385
 ajp_portGeo=9395
 
+file_path=0 # tomcat的server.xml路径
+file_DbName=0 # 临时保存文件中数据库名称
+cat_port=0 # 临时保存截取端口的内容
+port_name=0 # 临时保存端口名称
+port_no=0 # 临时保存端口号
+
 # ================================================================ tomcat_aaron配置 ================================================================
-if [ -d $all_packages_path/$tomcat_name ]; then
-	# 拷贝修改权限
-	if [ -d $insatll_path/$tomcat_name ]; then
-		echo -e "\033[32m$insatll_path/$tomcat_name 已经存在\033[0m"
+configTomcat(){
+	if [ -d $all_packages_path/$tomcat_name ]; then
+		# 拷贝修改权限
+		if [ -d $insatll_path/$tomcat_name ]; then
+			echo -e "\033[32m$insatll_path/$tomcat_name 已经存在\033[0m"
+		else
+			echo -e "\033[32m开始拷贝$tomcat_name到$insatll_path 请等待...\033[0m"
+			echo "123456"|sudo -s cp -r $all_packages_path/$tomcat_name $insatll_path
+			echo "123456"|sudo -s chmod -R 777 $insatll_path/$tomcat_name
+		fi
+
+		echo -e "\033[32m开始文件配置，请等待...\033[0m"
+		sleep 1
+
+		# 配置第一个数据库及IP
+		# 获取原文件camo数据库名称
+		catFile=`cat $filePath_tomcat | grep url`
+		aaa=${catFile%%\?*}
+		firstDbName=${aaa##*/}
+		file_DbName=$firstDbName
+		configTomcatDbAndIP
+		sleep 1
+
+		# 配置第二个数据库及IP
+		# 获取原文件aaron数据库名称
+		#catFile=`cat $filePath_tomcat | grep url`
+		bbb=${catFile%\?*}
+		secondDbName=${bbb##*/}
+		file_DbName=$secondDbName
+		configTomcatDbAndIP
+
+		file_path=$filePath_tomcatPort
+		configTomcatPort
+		
 	else
-		echo -e "\033[32m开始拷贝$tomcat_name到$insatll_path 请等待...\033[0m"
-		echo "123456"|sudo -s cp -r $all_packages_path/$tomcat_name $insatll_path
-		echo "123456"|sudo -s chmod -R 777 $insatll_path/$tomcat_name
-	fi
+		echo -e "\033[31m错误：$tomcat_name不存在，先将文件夹拷贝至$all_packages_path路径下！\033[0m"
+	fi	
+}
 
-	echo -e "\033[32m开始文件配置，请等待...\033[0m"
-	sleep 1
-	# 截取文件
-	catFile=`cat $filePath_tomcat | grep url`
-
-	# 获取原文件camo数据库名称
-	bbb=${catFile%%\?*}
-	firstDbName=${bbb##*/}
-
-	cat2File=`cat $filePath_tomcat | grep $firstDbName`
-	
+configTomcatDbAndIP(){
+	cat2File=`cat $filePath_tomcat | grep $file_DbName`
 	#获取指定字符串的行号
-	line=`sed -n '/'$(echo $firstDbName)'/=' $filePath_tomcat`
-
+	line=`sed -n '/'$(echo $file_DbName)'/=' $filePath_tomcat`
 	# 判断第一个数据库名称
-	if [[ "$firstDbName" =~ camo.* ]]; then
+	if [[ "$file_DbName" =~ camo.* ]]; then
+		echo -e "\033[32m将数据库：$file_DbName 修改为：$camo_db_name，IP修改为：$camo_IP_database\033[0m"
 		newInfo=\\${cat2File%%//*}//${camo_IP_database}:3306/${camo_db_name}?${cat2File##*\?}
 		# 删除这行,插入新字符串
 		sed -i "$line d" $filePath_tomcat
 		sed -i "$line i$newInfo" $filePath_tomcat
 	else
+		echo -e "\033[32m将数据库：$file_DbName 修改为：$aaron_db_name，IP修改为：$aaron_IP_database\033[0m"
 		newInfo=\\${cat2File%%//*}//${aaron_IP_database}:3306/${aaron_db_name}?${cat2File##*\?}
 		# 删除这行,插入新字符串
 		sed -i "$line d" $filePath_tomcat
 		sed -i "$line i$newInfo" $filePath_tomcat
 	fi
+}
 
-	# 重新截取文件
-	catFile=`cat $filePath_tomcat | grep url`
-	# 获取原文件aaron数据库名称
-	ddd=${catFile%\?*}
-	secondDbName=${ddd##*/}
-
-	cat3File=`cat $filePath_tomcat | grep $secondDbName`
-	#获取指定字符串的行号
-	line2=`sed -n '/'$(echo $secondDbName)'/=' $filePath_tomcat`
-
-	# 判断第二个数据库名称
-	if [[ "$secondDbName" =~ camo.* ]]; then
-		newInfo=\\${cat3File%%//*}//${camo_IP_database}:3306/${camo_db_name}?${cat3File##*\?}
-		# 删除这行,插入新字符串
-		sed -i "$line2 d" $filePath_tomcat
-		sed -i "$line2 i$newInfo" $filePath_tomcat
-
-	else
-		newInfo=\\${cat3File%%//*}//${aaron_IP_database}:3306/${aaron_db_name}?${cat3File##*\?}
-		# 删除这行,插入新字符串
-		sed -i "$line2 d" $filePath_tomcat
-		sed -i "$line2 i$newInfo" $filePath_tomcat
-	fi
-
-
+configTomcatPort(){
 	# 修改端口
 	# shutdown端口
-	catShutdownPort=`cat $filePath_tomcatPort | grep "<Server port=\"....\" shutdown=\"SHUTDOWN\">"`
-	eee=${catShutdownPort#*\"}
-	shutdownPort=${eee%%\"*}
-	if [ "$shutdownPort" == "$shutdown_portTomcat" ]; then
-		echo -e "\033[32mtomcat的SHUTDOWN端口已经配置\033[0m"
-	else
-		echo "123456"|sudo -s sed -i 's/'$(echo $shutdownPort)'/'$(echo $shutdown_portTomcat)'/g' $filePath_tomcatPort
-		if [ $? -eq 0 ]; then
-			echo -e "\033[32mtomcat的SHUTDOWN端口配置成功\033[0m"
-		else
-			echo -e "\033[31mtomcat的SHUTDOWN端口配置失败\033[0m"
-		fi
-	fi
+	catShutdownPort=`cat $file_path | grep "<Server port=\"....\" shutdown=\"SHUTDOWN\">"`
+	cat_port=$catShutdownPort
+	port_name=Shutdown
+	port_no=$shutdown_portTomcat
+	configPort
 
 	# http端口
-	catHTTPPort=`cat $filePath_tomcatPort | grep "<Connector connectionTimeout=\"20000\" port=\"....\" protocol=\"HTTP/1.1\" redirectPort=\"....\"/>"`
-	fff=${catHTTPPort#*port=\"}
-	HTTPPort=${fff%%\"*}
-	if [ "$HTTPPort" == "$http_portTomcat" ]; then
-		echo -e "\033[32mtomcat的HTTP端口已经配置\033[0m"
-	else
-		echo "123456"|sudo -s sed -i 's/'$(echo $HTTPPort)'/'$(echo $http_portTomcat)'/g' $filePath_tomcatPort
-		if [ $? -eq 0 ]; then
-			echo -e "\033[32mtomcat的HTTP端口配置成功\033[0m"
-		else
-			echo -e "\033[31mtomcat的HTTP端口配置失败\033[0m"
-		fi
-	fi
+	catHTTPPort=`cat $file_path | grep "<Connector connectionTimeout=\"20000\" port=\"....\" protocol=\"HTTP/1.1\" redirectPort=\"....\"/>"`
+	cat_port=$catHTTPPort
+	port_name=HTTP
+	port_no=$http_portTomcat
+	configPort
 
 	# redirect端口
 	ggg=${catHTTPPort#*redirectPort=\"}
 	redirectPort=${ggg%%\"*}
 
 	# AJP端口
-	catAjpPort=`cat $filePath_tomcatPort | grep "<Connector port=\"....\" protocol=\"AJP/1.3\" redirectPort=\"....\"/>"`
-	hhh=${catAjpPort#*port=\"}
-	AJPPort=${hhh%%\"*}
-	if [ "$AJPPort" == "$ajp_portTomcat" ]; then
-		echo -e "\033[32mtomcat的AJP端口已经配置\033[0m"
+	catAjpPort=`cat $file_path | grep "<Connector port=\"....\" protocol=\"AJP/1.3\" redirectPort=\"....\"/>"`
+	cat_port=$catAjpPort
+	port_name=AJP
+	port_no=$ajp_portTomcat
+	configPort
+
+}
+
+configPort(){
+	hhh=${cat_port#*port=\"}
+	port=${hhh%%\"*}
+	if [ "$port" == "$portNo" ]; then
+		echo -e "\033[32mtomcat的$portName端口已经配置\033[0m"
 	else
-		echo "123456"|sudo -s sed -i 's/'$(echo $AJPPort)'/'$(echo $ajp_portTomcat)'/g' $filePath_tomcatPort
+		echo "123456"|sudo -s sed -i 's/'$(echo $port)'/'$(echo $portNo)'/g' $file_path
 		if [ $? -eq 0 ]; then
-			echo -e "\033[32mtomcat的AJP端口配置成功\033[0m"
+			echo -e "\033[32mtomcat的$portName端口配置成功\033[0m"
 		else
-			echo -e "\033[31mtomcat的AJP端口配置失败\033[0m"
+			echo -e "\033[31mtomcat的$portName端口配置失败\033[0m"
 		fi
 	fi
-else
-	echo -e "\033[31m错误：$tomcat_name不存在，先将文件夹拷贝至$all_packages_path路径下！\033[0m"
-fi
+}
+
 
 # ================================================================ tomcat_geoLayer配置 ================================================================
-if [ -d $all_packages_path/$geoLayer_name ]; then
-	# 拷贝修改权限
-	if [ -d $insatll_path/$geoLayer_name ]; then
-		echo -e "\033[32m$insatll_path/$geoLayer_name 已经存在\033[0m"
-	else
-		echo -e "\033[32m开始拷贝$geoLayer_name到$insatll_path 请等待...\033[0m"
-		echo "123456"|sudo -s cp -r $all_packages_path/$geoLayer_name $insatll_path
-		echo "123456"|sudo -s chmod -R 777 $insatll_path/$geoLayer_name
-	fi
-	
-	echo -e "\033[32m开始文件配置，请等待...\033[0m"
-	sleep 1
-	
-	# 截取文件
-	catGeoCamoIP=`cat $filePath_geoCamo | grep host`
-	# 获取原文件camo数据库IP
-	eee=${catGeoCamoIP#*>}
-	geoCamoIP=${eee%%<*}
-	if [ "$geoCamoIP" == "$camo_IP_database" ]; then
-		echo -e "\033[32mgeoserver的camo数据库IP已经配置\033[0m"
-	else
-		echo "123456"|sudo -s sed -i 's/'$(echo $geoCamoIP)'/'$(echo $camo_IP_database)'/g' $filePath_geoCamo
-		if [ $? -eq 0 ]; then
-			echo -e "\033[32mgeoserver的camo数据库IP配置成功\033[0m"
+configTomcatGeo(){
+	if [ -d $all_packages_path/$geoLayer_name ]; then
+		# 拷贝修改权限
+		if [ -d $insatll_path/$geoLayer_name ]; then
+			echo -e "\033[32m$insatll_path/$geoLayer_name 已经存在\033[0m"
 		else
-			echo -e "\033[31mgeoserver的camo数据库IP配置失败\033[0m"
+			echo -e "\033[32m开始拷贝$geoLayer_name到$insatll_path 请等待...\033[0m"
+			echo "123456"|sudo -s cp -r $all_packages_path/$geoLayer_name $insatll_path
+			echo "123456"|sudo -s chmod -R 777 $insatll_path/$geoLayer_name
 		fi
-	fi
-
-	# 截取文件
-	catGeoCamoDb=`cat $filePath_geoCamo | grep database`
-	# 获取原文件camo数据库名称
-	fff=${catGeoCamoDb#*>}
-	geoCamoDbName=${fff%%<*}
-	if [ "$geoCamoDbName" == "$camo_db_name" ]; then
-		echo -e "\033[32mgeoserver的camo数据库名称已经配置\033[0m"
-	else
-		echo "123456"|sudo -s sed -i 's/'$(echo $geoCamoDbName)'/'$(echo $camo_db_name)'/g' $filePath_geoCamo
-		if [ $? -eq 0 ]; then
-			echo -e "\033[32mgeoserver的camo数据库名称配置成功\033[0m"
+		
+		echo -e "\033[32m开始文件配置，请等待...\033[0m"
+		sleep 1
+		
+		# 截取文件
+		catGeoCamoIP=`cat $filePath_geoCamo | grep host`
+		# 获取原文件camo数据库IP
+		eee=${catGeoCamoIP#*>}
+		geoCamoIP=${eee%%<*}
+		if [ "$geoCamoIP" == "$camo_IP_database" ]; then
+			echo -e "\033[32mgeoserver的camo数据库IP已经配置\033[0m"
 		else
-			echo -e "\033[31mgeoserver的camo数据库名称配置失败\033[0m"
+			echo "123456"|sudo -s sed -i 's/'$(echo $geoCamoIP)'/'$(echo $camo_IP_database)'/g' $filePath_geoCamo
+			if [ $? -eq 0 ]; then
+				echo -e "\033[32mgeoserver的camo数据库IP配置成功\033[0m"
+			else
+				echo -e "\033[31mgeoserver的camo数据库IP配置失败\033[0m"
+			fi
 		fi
-	fi
 
-	# 截取文件
-	catGeoAaronIP=`cat $filePath_geoAaron | grep host`
-	# 获取原文件aaron数据库IP
-	ggg=${catGeoAaronIP#*>}
-	geoAaronIP=${ggg%%<*}
-	if [ "$geoAaronIP" == "$aaron_IP_database" ]; then
-		echo -e "\033[32mgeoserver的aaron数据库IP已经配置\033[0m"
-	else
-		echo "123456"|sudo -s sed -i 's/'$(echo $geoAaronIP)'/'$(echo $aaron_IP_database)'/g' $filePath_geoAaron
-		if [ $? -eq 0 ]; then
-			echo -e "\033[32mgeoserver的aaron数据库IP配置成功\033[0m"
+		# 截取文件
+		catGeoCamoDb=`cat $filePath_geoCamo | grep database`
+		# 获取原文件camo数据库名称
+		fff=${catGeoCamoDb#*>}
+		geoCamoDbName=${fff%%<*}
+		if [ "$geoCamoDbName" == "$camo_db_name" ]; then
+			echo -e "\033[32mgeoserver的camo数据库名称已经配置\033[0m"
 		else
-			echo -e "\033[31mgeoserver的aaron数据库IP配置失败\033[0m"
+			echo "123456"|sudo -s sed -i 's/'$(echo $geoCamoDbName)'/'$(echo $camo_db_name)'/g' $filePath_geoCamo
+			if [ $? -eq 0 ]; then
+				echo -e "\033[32mgeoserver的camo数据库名称配置成功\033[0m"
+			else
+				echo -e "\033[31mgeoserver的camo数据库名称配置失败\033[0m"
+			fi
 		fi
-	fi
 
-	# 截取文件
-	catGeoAaronDb=`cat $filePath_geoAaron | grep database`
-	# 获取原文件aaron数据库IP
-	hhh=${catGeoAaronDb#*>}
-	geoAaronDbName=${hhh%%<*}
-	if [ "$geoAaronDbName" == "$aaron_db_name" ]; then
-		echo -e "\033[32mgeoserver的aaron数据库名称已经配置\033[0m"
-	else
-		echo "123456"|sudo -s sed -i 's/'$(echo $geoAaronDbName)'/'$(echo $aaron_db_name)'/g' $filePath_geoAaron
-		if [ $? -eq 0 ]; then
-			echo -e "\033[32mgeoserver的aaron数据库名称配置成功\033[0m"
+		# 截取文件
+		catGeoAaronIP=`cat $filePath_geoAaron | grep host`
+		# 获取原文件aaron数据库IP
+		ggg=${catGeoAaronIP#*>}
+		geoAaronIP=${ggg%%<*}
+		if [ "$geoAaronIP" == "$aaron_IP_database" ]; then
+			echo -e "\033[32mgeoserver的aaron数据库IP已经配置\033[0m"
 		else
-			echo -e "\033[31mgeoserver的aaron数据库名称配置失败\033[0m"
+			echo "123456"|sudo -s sed -i 's/'$(echo $geoAaronIP)'/'$(echo $aaron_IP_database)'/g' $filePath_geoAaron
+			if [ $? -eq 0 ]; then
+				echo -e "\033[32mgeoserver的aaron数据库IP配置成功\033[0m"
+			else
+				echo -e "\033[31mgeoserver的aaron数据库IP配置失败\033[0m"
+			fi
 		fi
-	fi
 
-	# 修改端口
-	# shutdown端口
-	catShutdownPort=`cat $filePath_geoPort | grep "<Server port=\"....\" shutdown=\"SHUTDOWN\">"`
-	eee=${catShutdownPort#*\"}
-	shutdownPort=${eee%%\"*}
-	if [ "$shutdownPort" == "$shutdown_portGeo" ]; then
-		echo -e "\033[32mtomcat的SHUTDOWN端口已经配置\033[0m"
-	else
-		echo "123456"|sudo -s sed -i 's/'$(echo $shutdownPort)'/'$(echo $shutdown_portGeo)'/g' $filePath_geoPort
-		if [ $? -eq 0  ]; then
-			echo -e "\033[32mtomcat的SHUTDOWN端口配置成功\033[0m"
+		# 截取文件
+		catGeoAaronDb=`cat $filePath_geoAaron | grep database`
+		# 获取原文件aaron数据库IP
+		hhh=${catGeoAaronDb#*>}
+		geoAaronDbName=${hhh%%<*}
+		if [ "$geoAaronDbName" == "$aaron_db_name" ]; then
+			echo -e "\033[32mgeoserver的aaron数据库名称已经配置\033[0m"
 		else
-			echo -e "\033[31mtomcat的SHUTDOWN端口配置失败\033[0m"
+			echo "123456"|sudo -s sed -i 's/'$(echo $geoAaronDbName)'/'$(echo $aaron_db_name)'/g' $filePath_geoAaron
+			if [ $? -eq 0 ]; then
+				echo -e "\033[32mgeoserver的aaron数据库名称配置成功\033[0m"
+			else
+				echo -e "\033[31mgeoserver的aaron数据库名称配置失败\033[0m"
+			fi
 		fi
-	fi
 
-	# http端口
-	catHTTPPort=`cat $filePath_geoPort | grep "<Connector connectionTimeout=\"20000\" port=\"....\" protocol=\"HTTP/1.1\" redirectPort=\"....\"/>"`
-	fff=${catHTTPPort#*port=\"}
-	HTTPPort=${fff%%\"*}
-	if [ "$HTTPPort" == "$http_portGeo" ]; then
-		echo -e "\033[32mtomcat的HTTP端口已经配置\033[0m"
+		file_path=$filePath_geoPort
+		configTomcatPort
+		
 	else
-		echo "123456"|sudo -s sed -i 's/'$(echo $HTTPPort)'/'$(echo $http_portGeo)'/g' $filePath_geoPort
-		if [ $? -eq 0 ]; then
-			echo -e "\033[32mtomcat的HTTP端口配置成功\033[0m"
-		else
-			echo -e "\033[31mtomcat的HTTP端口配置失败\033[0m"
-		fi
+		echo -e "\033[31m错误：$geoLayer_name不存在，先将文件夹拷贝至$all_packages_path路径下！\033[0m"
 	fi
+}
 
-	#redirect端口
-	ggg=${catHTTPPort#*redirectPort=\"}
-	redirectPort=${ggg%%\"*}
-
-	# AJP端口
-	catAjpPort=`cat $filePath_geoPort | grep "<Connector port=\"....\" protocol=\"AJP/1.3\" redirectPort=\"....\"/>"`
-	hhh=${catAjpPort#*port=\"}
-	AJPPort=${hhh%%\"*}
-	if [ "$AJPPort" == "$ajp_portGeo" ]; then
-		echo -e "\033[32mtomcat的AJP端口已经配置\033[0m"
-	else
-		echo "123456"|sudo -s sed -i 's/'$(echo $AJPPort)'/'$(echo $ajp_portGeo)'/g' $filePath_geoPort
-		if [ $? -eq 0 ]; then
-			echo -e "\033[32mtomcat的AJP端口配置成功\033[0m"
-		else
-			echo -e "\033[31mtomcat的AJP端口配置失败\033[0m"
-		fi
-	fi
-else
-	echo -e "\033[31m错误：$geoLayer_name不存在，先将文件夹拷贝至$all_packages_path路径下！\033[0m"
-fi
+# ================================================================ 主函数 ================================================================
+menu(){
+	configTomcat
+	configTomcatGeo
+}
+menu
